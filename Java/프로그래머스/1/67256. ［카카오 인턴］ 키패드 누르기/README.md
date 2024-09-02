82ee9292da504db7d4deecf375fbbc0bf890fd08
myqewr <97663486+myqewr@users.noreply.github.com>
2024-09-02 17:14:18 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 4.14 ms, Memory: 78.8 MB -BaekjoonHub

--- Java/프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ Java/프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=java) 
 
 ### 성능 요약
 
-메모리: 71.1 MB, 시간: 4.58 ms
+메모리: 78.8 MB, 시간: 4.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 02일 17:09:12
+2024년 09월 02일 17:14:18
 
 ### 문제 설명
 
--- Java/프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ Java/프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -3,24 +3,23 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers, String hand) {
     
-        
-        //Map<Integer,Map<Integer,Integer>> distanceMap = new HashMap<>();
-        
         int[][] nums = {{1,2,3},
                         {4,5,6},
                         {7,8,9}, 
                         {10,0,11}};
        
         int[][] distance = new int[12][2];
+        
         for(int x=0;x<4;x++){
+            
             for(int y=0;y<3;y++){
-                distance[nums[x][y]] = new int[]{x,y};
-            }
+                distance[nums[x][y]] = new int[]{x,y};}
         }
         
         String answer = "";
         int left = 10;
         int right = 11;
+        
         for(int num : numbers){
         
             if(distance[num][1]==2){
@@ -33,33 +32,26 @@ class Solution {
             }
             
             else {
-            int left_d =  Math.abs(distance[num][0] - distance[left][0])  
+                int left_d =  Math.abs(distance[num][0] - distance[left][0])  
                             +Math.abs(distance[left][1] - distance[num][1]);
-            int right_d =  Math.abs(distance[num][0] - distance[right][0])  
+                int right_d =  Math.abs(distance[num][0] - distance[right][0])  
                             +Math.abs(distance[num][1] - distance[right][1]);
             
-            if(left_d<right_d){
-                left = num;
-                answer = answer+ "L";
-            }
-            else if(left_d>right_d){
-                right = num;
-                answer = answer+ "R";
-            }
-            else {
-                if(hand.equals("right")){
+                if(left_d<right_d){
+                    left = num;
+                    answer = answer+ "L";
+                }
+                else if(left_d>right_d){
                     right = num;
                     answer = answer+ "R";
                 }
                 else {
-                    left = num;
-                    answer = answer+ "L";
-                }
-            }
-            
-        }   
-            }
-        
+                    if(hand.equals("right")){
+                        right = num;
+                        answer = answer+ "R";}
+                    else {
+                        left = num;
+                        answer = answer+ "L";}}}}
         return answer;
     }
 }
