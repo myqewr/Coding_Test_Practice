ae0052ce1f9806f587d64200f47339c6dc7739ea
김민영 <97663486+myqewr@users.noreply.github.com>
2024-10-09 05:17:50 +0900

    [level 3] Title: 가장 먼 노드, Time: 1880.33 ms, Memory: 106 MB -BaekjoonHub

--- Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,23 +1,24 @@
+
 import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
-        
+
         ArrayList<Integer>[] gr = new ArrayList[n+1];
         ArrayList<Integer> visited = new ArrayList<>();
         int[] distance = new int[n+1];
-        
+
         for(int i=1;i<=n;i++) gr[i] = new ArrayList<>();
-        
+
         for(int[] ed : edge) {
             gr[ed[0]].add(ed[1]);
             gr[ed[1]].add(ed[0]);
         }
-        
+
         ArrayList<Integer> nextNode = new ArrayList<>();
         Queue<Integer> currentNode = new LinkedList<>();
-        
+
         currentNode.add(1);
-        
+
         for(int i=0;i<=n;i++){
             if(currentNode.size()==0){break;}
             while(currentNode.size()!=0){
@@ -30,12 +31,12 @@ class Solution {
             currentNode.addAll(nextNode);
             nextNode.clear();
         }
-        
-        
+
+
         for(int i = n;i>=1;i--){
             if(distance[i]!=0){return distance[i];}
         }
         return 0;
-        
+
     }
 }
