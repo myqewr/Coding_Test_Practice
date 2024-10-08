79ad0e7b8a8101801d21aca1d7a218181446a1bc
김민영 <97663486+myqewr@users.noreply.github.com>
2024-10-09 05:40:43 +0900

    [level 3] Title: 가장 먼 노드, Time: 342.87 ms, Memory: 104 MB -BaekjoonHub

--- Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,42 +1,44 @@
+import java.util.ArrayList;
 
-import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
+        ArrayList<Integer>[] path = new ArrayList[n];
+        ArrayList<Integer> bfs = new ArrayList<Integer>();
+        int answer = 0;
+        int[] dist = new int[n];
+        dist[0] = 1;
+        int max = 0;
 
-        ArrayList<Integer>[] gr = new ArrayList[n+1];
-        ArrayList<Integer> visited = new ArrayList<>();
-        int[] distance = new int[n+1];
-
-        for(int i=1;i<=n;i++) gr[i] = new ArrayList<>();
-
-        for(int[] ed : edge) {
-            gr[ed[0]].add(ed[1]);
-            gr[ed[1]].add(ed[0]);
+        for(int i = 0; i < edge.length; i++) {
+            int num1 = edge[i][0] - 1;
+            int num2 = edge[i][1] - 1;
+            if(path[num1] == null)
+                path[num1] = new ArrayList<Integer>();
+            if(path[num2] == null)
+                path[num2] = new ArrayList<Integer>();
+            path[num1].add(num2);
+            path[num2].add(num1);
         }
 
-        ArrayList<Integer> nextNode = new ArrayList<>();
-        Queue<Integer> currentNode = new LinkedList<>();
-
-        currentNode.add(1);
-
-        for(int i=0;i<=n;i++){
-            if(currentNode.size()==0){break;}
-            while(currentNode.size()!=0){
-                int node = currentNode.poll();
-                if(visited.contains(node)){continue;}
-                nextNode.addAll(gr[node]);
-                visited.add(node);
-                distance[i]++;
+        bfs.add(0);
+        while(!bfs.isEmpty()) {
+            int idx = bfs.get(0);
+            bfs.remove(0);
+            while(!path[idx].isEmpty()) {
+                int num = path[idx].get(0);
+                path[idx].remove(0);
+                bfs.add(num);
+                if(dist[num] == 0) {
+                    dist[num] = dist[idx] + 1;
+                    max = dist[num];
+                }
             }
-            currentNode.addAll(nextNode);
-            nextNode.clear();
+        }
+        for(int i = 0; i < n; i++) {
+            if(dist[i] == max)
+                answer++;
         }
 
-
-        for(int i = n;i>=1;i--){
-            if(distance[i]!=0){return distance[i];}
-        }
-        return 0;
-
+        return answer;
     }
 }
