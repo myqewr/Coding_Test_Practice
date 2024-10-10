e460219dcf629d05dfeebda747fc1b34a24d6c32
Minyoung Kim <97663486+myqewr@users.noreply.github.com>
2024-10-10 21:46:47 +0900

    [level 3] Title: 순위, Time: 15.39 ms, Memory: 84 MB -BaekjoonHub

--- Java/프로그래머스/3/49191. 순위/README.md
+++ Java/프로그래머스/3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=java) 
 
 ### 성능 요약
 
-메모리: 93.9 MB, 시간: 11.72 ms
+메모리: 84 MB, 시간: 15.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 10일 21:43:25
+2024년 10월 10일 21:46:44
 
 ### 문제 설명
 
--- Java/프로그래머스/3/49191. 순위/순위.java
+++ Java/프로그래머스/3/49191. 순위/순위.java
@@ -16,6 +16,8 @@ class Solution {
             for(int i=1;i<=n;i++){
                 for(int j=1;j<=n;j++){
                     if(gr[i][k]+gr[k][j]==2){
+                        //i가 k를 이겼고, k가 i를 이겼으면 i가 k를 이긴 것.
+                        //승리 그래프 update + i와 k의 관계를 알 수 있는지를 기록하는 dp도 수정. i,j/ j,i 모두 관계를 알고 있으므로 두개 다 수정.
                         gr[i][j]=1;
                         dp[i][j] =1;
                         dp[j][i] =1;
