9af40675d4394bae4842e8eb91bc3faf4fd79897
Minyoung Kim <97663486+myqewr@users.noreply.github.com>
2024-10-13 03:20:40 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 17.33 ms, Memory: 89.6 MB -BaekjoonHub

--- Java/프로그래머스/3/17678. ［1차］ 셔틀버스/README.md
+++ Java/프로그래머스/3/17678. ［1차］ 셔틀버스/README.md
@@ -1,10 +1,10 @@
 # [level 3] [1차] 셔틀버스 - 17678 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17678) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17678#qna) 
 
 ### 성능 요약
 
-메모리: 94.4 MB, 시간: 25.24 ms
+메모리: 89.6 MB, 시간: 17.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 13일 02:19:37
+2024년 10월 13일 03:20:38
 
 ### 문제 설명
 
--- Java/프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ Java/프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -1,76 +1,45 @@
 import java.util.*;
-import java.util.stream.Collectors.*;
+import java.util.stream.Collectors;
 class Solution {
     public String solution(int n, int t, int m, String[] timetable) {
         
-        Set<Integer> timeSet = new TreeSet<>();
-        int[] timeArray = new int[24*60];
-        
-        Arrays.stream(timetable).forEach(x->{
-            int converted = timeToMin(x);
-            timeArray[converted]++;
-            timeSet.add(converted);
-        });
-        
-        int start = 0;
-        int end = 24*60-1;
-        
-        System.out.println(valid(timeSet,timeArray,540,n,t,m));
-        
-        while(start<end){
+        List<Integer> timeTable = Arrays.stream(timetable).map(x->timeToMin(x)).collect(Collectors.toList());
+        Collections.sort(timeTable);
+
+        int index = 0;
+        int lastArrival = 0;
+        for(int i=0; i<n;i++){
+            int onCount = 0;
+            int arrivalTime =9*60+i*t;
             
-            int mid = (start+end)/2;
-            
-            if(valid(timeSet,timeArray,mid,n,t,m)){
-                start = mid+1;
+            while(index<timeTable.size()){
+                int currentCrew = timeTable.get(index);
+                if(currentCrew>arrivalTime){break;}
+                onCount++;
+                index++;
+                if(onCount==m){
+                    lastArrival = currentCrew;
+                    break;
+                }
             }
-            else{
-                end = mid;
-            } 
+            if(i==n-1){
+                if(onCount==m){lastArrival-=1;}
+                else lastArrival = arrivalTime;
+            }
         }
-        
-        String hour = String.valueOf((start-1)/60);
+
+        String hour = String.valueOf(lastArrival/60);
         hour = hour.length()==1?"0"+hour:hour;
-        String min = String.valueOf((start-1)%60);
+        String min = String.valueOf(lastArrival%60);
         min = min.length()==1?"0"+min:min;
         return hour+":"+min;
         
     }
-    
     
     //시간을 분으로 환산
     public int timeToMin(String time){
         int hour = Integer.valueOf(time.split(":")[0]);
         int min = Integer.valueOf(time.split(":")[1]);
         return hour*60+min;
-    }
-        
-    public Boolean valid(Set<Integer> set,int[] timeArrayOriginal,int time, int n, int t, int m){
-        Set<Integer> timeSet = new TreeSet<>(set);
-        timeSet.add(time);
-        
-        Integer[] line = timeSet.toArray(new Integer[timeSet.size()]);
-        int[] timeArray = timeArrayOriginal.clone();
-        timeArray[time]++;
-        
-        int index = 0;
-        for(int i=0; i<n;i++){
-            int currentMember = 0;
-            int arrivalTime =9*60+i*t;
-            
-            while(index<line.length){
-                int value = line[index];
-                if(value>arrivalTime){break;}
-                currentMember+=timeArray[value];
-                if(currentMember>m){
-                    timeArray[value]= currentMember - m;
-                    break;
-                }
-                if(value==time){return true;}
-                index++;
-            } 
-        }
-        return false;
-     
     }
 }
