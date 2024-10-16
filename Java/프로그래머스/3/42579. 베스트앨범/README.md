1e4518c35efe70926a581032e891c7ac0e0dd019
Minyoung Kim <97663486+myqewr@users.noreply.github.com>
2024-10-16 10:42:16 +0900

    [level 3] Title: 베스트앨범, Time: 1.23 ms, Memory: 75.3 MB -BaekjoonHub

--- Java/프로그래머스/3/42579. 베스트앨범/README.md
+++ Java/프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 78.4 MB, 시간: 6.23 ms
+메모리: 75.3 MB, 시간: 1.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 16일 00:21:40
+2024년 10월 16일 10:42:14
 
 ### 문제 설명
 
--- Java/프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ Java/프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,50 +1,63 @@
 import java.util.*;
+import java.util.Collections;
 import java.util.stream.Collectors;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         
-        Map<String,Integer> totalNumPerGen = new HashMap<>();
-        Map<String,List<Integer>> songListPerGen = new HashMap<>();
+        Map<String,Integer> genAndNum = new HashMap<>();
+        Map<String,Object> genAndSong= new HashMap<>();
         for(int i=0;i<genres.length;i++){
             String gen = genres[i];
-            int currentNum = totalNumPerGen.getOrDefault(gen,0);
-            totalNumPerGen.put(gen,currentNum+plays[i]);
+            int currentNum = genAndNum.getOrDefault(gen,0);
+            genAndNum.put(gen,currentNum+plays[i]);
             
-            List<Integer> songList = songListPerGen.getOrDefault(gen,new ArrayList<Integer>());
-            songList.add(i);
-            songListPerGen.put(gen, songList);
-        }
-        List<Integer> totalNums = totalNumPerGen.values().stream().sorted((a,b)->b-a).collect(Collectors.toList());
-        
-        
-        List<String> topGenres = new ArrayList<>();
-        for(int i=0;i<totalNums.size();i++){
-            int topGenreNum = totalNums.get(i);
-            for(String key : totalNumPerGen.keySet()){
-                if(totalNumPerGen.get(key) == topGenreNum){
-                    topGenres.add(key);
-                }
-            }
+            Map<Integer,Integer> songInfo = (HashMap<Integer,Integer>)genAndSong.getOrDefault(gen,new HashMap<Integer,Integer>());
+            songInfo.put(i,plays[i]);
+            genAndSong.put(gen,songInfo);
         }
         
         List<Integer> answer = new ArrayList<>();
+        
+        List<String> topGenres = sortByKey(genAndNum);
+        
         for(String gen : topGenres){
-            List<Integer> topSongs = songListPerGen.get(gen).stream().sorted().collect(Collectors.toList());;
-            for(int j=0;j<2;j++){
-                int max = 0;
-                int maxNum = 0;
-                for(int songNum : topSongs){
-                    if(plays[songNum]>max){
-                        max = plays[songNum];
-                        maxNum = songNum;
-                    }
-                }
-                answer.add(maxNum);
-                topSongs.remove((Integer)maxNum);
-                if(topSongs.size()==0){break;}
-            }
+            
+            List<Integer> values = sortByKey((HashMap<Integer,Integer>)genAndSong.get(gen));
+            
+            answer.add(values.get(0));
+            if(values.size()>=2){answer.add(values.get(1));}
+            
         }
-        return answer.stream().mapToInt(i->i).toArray(); 
+        
+        int answerArray[] = new int[answer.size()];
+        for(int i=0;i<answer.size();i++){
+            answerArray[i] = answer.get(i);
+        }
+        return answerArray;
+        
+        //Stream의 toArray는 Collection의 toArray보다 속도가 느리고, 메모리 소비가 많음.
+        //결과값을 int형으로 반환해야하는 경우, Collection값이 Integer라면 int형으로 변환 필요. -> stream돌며 형변환 -> 결국엔 stream
+        //따라서 그냥 반복문을 도는게 더 빠르다.
+        //return answer.stream().mapToInt(i->i).toArray(); 
+    }
+    
+    
+    List sortByKey(Map map){
+            
+        List keySet = new ArrayList<>();
+        keySet.addAll(map.keySet());
+            
+        Collections.sort(keySet,new Comparator(){
+                
+            public int compare(Object k1, Object k2){
+                    
+                Object v1 = map.get(k1);
+                Object v2 = map.get(k2);
+                    
+                return ((Comparable)v2).compareTo(v1); 
+            }});
+            
+        return keySet;
     }
 }
  
