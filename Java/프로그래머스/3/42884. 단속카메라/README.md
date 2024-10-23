f89ba6dbf04e8eb7f4110180be3fbb5f59ec3909
Minyoung Kim <97663486+myqewr@users.noreply.github.com>
2024-10-23 12:43:38 +0900

    [level 3] Title: 단속카메라, Time: 8.93 ms, Memory: 53.9 MB -BaekjoonHub

--- Java/프로그래머스/3/42884. 단속카메라/README.md
+++ Java/프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=java) 
 
 ### 성능 요약
 
-메모리: 56.2 MB, 시간: 7.42 ms
+메모리: 53.9 MB, 시간: 8.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 23일 12:33:52
+2024년 10월 23일 12:43:36
 
 ### 문제 설명
 
--- Java/프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ Java/프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,21 +2,13 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         
-        int len = routes.length;
         Arrays.sort(routes,(a,b)->a[1]-b[1]);
-        
-        int num=0;
-        int x=0;
-        boolean bool  = true;
-        while(bool){
-            num++;
-            int out = routes[x][1];
-            for(int i = x; i<routes.length;i++){
-                if(out<routes[i][0] || out>routes[i][1]){
-                    x = i;
-                    break;
-                }
-                if(i==routes.length-1){bool = false;}
+        int num=1;
+        int out = 0;
+        for(int i = 1; i<routes.length;i++){
+            if(routes[out][1]<routes[i][0]){
+                num++;
+                out = i;
             }
         }
         return num;
