08b8b64602852b5ea24df821985e47617ed607f2
Minyoung Kim <97663486+myqewr@users.noreply.github.com>
2024-10-24 16:50:35 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 18.65 ms, Memory: 52.5 MB -BaekjoonHub

--- Java/프로그래머스/3/12904. 가장 긴 팰린드롬/README.md
+++ Java/프로그래머스/3/12904. 가장 긴 팰린드롬/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 긴 팰린드롬 - 12904 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12904) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12904?language=java) 
 
 ### 성능 요약
 
-메모리: 52 MB, 시간: 34.45 ms
+메모리: 52.5 MB, 시간: 18.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 24일 16:47:08
+2024년 10월 24일 16:50:33
 
 ### 문제 설명
 
--- Java/프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ Java/프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -2,13 +2,8 @@ import java.util.*;
 class Solution
 {
     public int solution(String s){
-        //string을 string 배열로 변환
-        char[] charString = s.toCharArray();
-        String[] str = new String[s.length()];
-
-        for(int i=0;i<s.length();i++){
-            str[i] = String.valueOf(charString[i]);}
         
+        char[] str = s.toCharArray();
         int max = 0;
         int len = s.length();
         for(int i=0;i<len;i++){
@@ -16,18 +11,18 @@ class Solution
             int left = i-1;
             int right =i+1;
             int lenOfOdd = 1;
-            while(left>=0 && right<len&& str[left].equals(str[right])){
+            while(left>=0 && right<len&& str[left]==str[right]){
                 lenOfOdd+=2;
                 left--;
                 right++;
             }
             //팰린드롬이 짝수일 경우
             int lenOfEven = 0;
-            if(i+1<len && str[i].equals(str[i+1])){
+            if(i+1<len && str[i]==str[i+1]){
                 lenOfEven = 2;
                 int l = i-1;
                 int r = i+2;
-                while(l>=0 && r<len && str[l].equals(str[r])){
+                while(l>=0 && r<len && str[l]==str[r]){
                     lenOfEven+=2;
                     l--;
                     r++;
