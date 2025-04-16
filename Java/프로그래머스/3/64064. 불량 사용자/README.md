a373e42e49c7d21dd42279b261037acb458dc148
Minyoung Kim <mygongjoo@gmail.com>
2025-04-16 20:11:21 +0900

    [level 3] Title: 불량 사용자, Time: 152.60 ms, Memory: 149 MB -BaekjoonHub

--- Java/프로그래머스/3/64064. 불량 사용자/README.md
+++ Java/프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
 
 ### 성능 요약
 
-메모리: 394 MB, 시간: 347.30 ms
+메모리: 149 MB, 시간: 152.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 01:46:10
+2025년 04월 16일 20:11:18
 
 ### 문제 설명
 
--- Java/프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ Java/프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,42 +1,54 @@
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.regex.Pattern;
 class Solution {
-    
-    public List<String> f(String ban, List<String> names){
-
-        ban = ban.replace("*","[\\w]");
+    public int solution(String[] user_id, String[] banned_id) {
         
-        List<String> banNameList = new ArrayList<>();
-        for(String name: names){
-            if(name.matches(ban)){banNameList.add(name);}
-        }
-        return banNameList;
+    
+        List<List<String>> result= dfs(0, Arrays.asList(user_id),banned_id,new ArrayList<>(),banned_id.length-1);        
+        
+        return (int)result.stream().distinct().count();
+    
     }
     
     
-    public  Map<List<String>,Integer> dfs(int nth, String[] banned, List<String> names, Map<List<String>,Integer> map, List<String> list){
-
-        if(banned.length==nth){
-            list = list.stream().sorted().collect(Collectors.toList());
-            map.put(list,1);
-            return map;
-        }
+    private List<List<String>> dfs(int index,List<String> candidates,String[]banned_id, List<String> list, int lastIndex){
+        if(index > lastIndex){
+            Collections.sort(list);
+            List<List<String>> result= new ArrayList<>(); 
+            result.add(list);
+            return result;}
         
-        String ban = banned[nth];
-        List<String> ban_list = f(ban, names);
-        for(String ban_name : ban_list){
-            List<String> new_names = new ArrayList<>(names);
-            new_names.remove(ban_name);
-            List<String> newList= new ArrayList<>(list);
-            newList.add(ban_name);
-            map = dfs(nth+1, banned, new_names,map,newList);
+        String text = banned_id[index];
+        long num =  candidates.stream().filter(c->isMatch(c,text)).count();
+        if(num==0) {return new ArrayList<>();}
+        
+        List<String> selected = candidates.stream().filter(c->isMatch(c,text)).collect(Collectors.toList());
+        
+        List<List<String>> set = new ArrayList<>();
+        for(String value : selected){
+            List<String> newCandidates = new ArrayList<>(candidates);
+            newCandidates.remove(value);
+            List<String> newList = new ArrayList<>(list);
+            newList.add(value);
+            List<List<String>> temp =dfs(index+1, newCandidates,banned_id,newList,lastIndex);
+            set.addAll(temp);
         }
-        return map;
-     }
+        return set;       
+    }
     
-    public int solution(String[] user_id, String[] banned_id) {
-        Map<List<String>,Integer> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
-        return dfs(0,banned_id,Arrays.asList(user_id),map,list).size();
+    public static boolean isMatch(String target,String pattern) {
+        // 길이가 다르면 무조건 false
+        if (pattern.length() != target.length()) return false;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char p = pattern.charAt(i);
+            char t = target.charAt(i);
+
+            if (p != '*' && p != t) {
+                return false; // 와일드카드가 아닌데 다르면 불일치
+            }
+        }
+        return true;
     }
 }
