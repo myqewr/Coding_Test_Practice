4b4f3afb76b97d64259157c5091be29a23d4d937
Minyoung Kim <mygongjoo@gmail.com>
2025-04-16 22:42:34 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 86.5 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,23 @@
 class Solution {
-    
-    public int[] dfs(int n, int[][]computers, int nth, int[] isChecked){
-        isChecked[nth]=1;
-        for(int i=0 ;i<n;i++){
-            if(i!=nth && computers[nth][i]==1 &&isChecked[i]==0){
-                isChecked = dfs(n,computers,i,isChecked);
-            }
+    public int solution(int n, int[][] computers) {
+        
+        int[] visited = new int[n];
+        int num= 0;
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){num++;dfs(visited,i,computers);}
         }
-        return isChecked;
+        
+        return num;
     }
     
-    
-    public int solution(int n, int[][] computers) {
-        int[] isChecked = new int[n];
-        int answer = 0;
-        for(int i=0;i<n;i++){
-            if(isChecked[i]==0){
-                isChecked = dfs(n,computers,i,isChecked);
-                answer +=1;
+    private void dfs(int[]visited,int node, int[][]computers){
+        int[] connected =  computers[node];
+        visited[node]=1;
+        for(int i=0;i<connected.length;i++){
+            if(connected[i]==1 && visited[i]==0){
+                dfs(visited,i,computers);
             }
         }
-        return answer;
+        return ;
     }
 }
