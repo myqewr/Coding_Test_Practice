1f5009e8e6b9feb8d795acdb60eb1b7b2589b3eb
Minyoung Kim <mygongjoo@gmail.com>
2025-04-18 16:37:14 +0900

    [level 3] Title: 가장 먼 노드, Time: 80.01 ms, Memory: 103 MB -BaekjoonHub

--- Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,44 +1,63 @@
-import java.util.ArrayList;
-
+import java.util.*;
 class Solution {
-    public int solution(int n, int[][] edge) {
-        ArrayList<Integer>[] path = new ArrayList[n];
-        ArrayList<Integer> bfs = new ArrayList<Integer>();
-        int answer = 0;
-        int[] dist = new int[n];
-        dist[0] = 1;
-        int max = 0;
-
-        for(int i = 0; i < edge.length; i++) {
-            int num1 = edge[i][0] - 1;
-            int num2 = edge[i][1] - 1;
-            if(path[num1] == null)
-                path[num1] = new ArrayList<Integer>();
-            if(path[num2] == null)
-                path[num2] = new ArrayList<Integer>();
-            path[num1].add(num2);
-            path[num2].add(num1);
+    public int solution(int n, int[][] edges) {
+        Map<Integer,List<Integer>> graph = new HashMap<>();//양방향 그래프
+        
+        int[] status = new int[n+1] ;
+        for(int i=2;i<=n;i++){
+            status[i] = Integer.MAX_VALUE;
         }
-
-        bfs.add(0);
-        while(!bfs.isEmpty()) {
-            int idx = bfs.get(0);
-            bfs.remove(0);
-            while(!path[idx].isEmpty()) {
-                int num = path[idx].get(0);
-                path[idx].remove(0);
-                bfs.add(num);
-                if(dist[num] == 0) {
-                    dist[num] = dist[idx] + 1;
-                    max = dist[num];
+        
+        for(int[] edge : edges){
+            List<Integer> adj1 =  graph.getOrDefault(edge[0],new ArrayList<>());
+            adj1.add(edge[1]);
+            graph.put(edge[0],adj1);
+            List<Integer> adj2 =  graph.getOrDefault(edge[1],new ArrayList<>());
+            adj2.add(edge[0]);
+            graph.put(edge[1],adj2);
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(p -> status[p]));
+        
+       
+        //1의 인접 노드는 초기화, 간선 수 1로 설정;
+        for(int adj : graph.get(1)){
+            status[adj] = 1;
+            pq.add(adj);
+        }
+        
+        int[] visited = new int[n+1];
+        visited[1] =1;
+        while(!pq.isEmpty()){
+            Integer node = pq.poll();
+            
+            if(visited[node]==1){continue;}
+            
+            visited[node] = 1;
+            
+            List<Integer> children = graph.get(node);
+            
+            for(int child :  children){
+                if(visited[child]==1){continue;}
+                pq.add(child);
+                if(status[child] > status[node] +1){
+                    status[child] = status[node]+1;
+            
                 }
             }
         }
-        for(int i = 0; i < n; i++) {
-            if(dist[i] == max)
-                answer++;
+        
+    
+        
+        int max = Arrays.stream(status).max().getAsInt();
+        
+        int answer =0;
+        for(int i=2;i<=n;i++){
+            if(max==status[i]){answer++;}
         }
-
+        
         return answer;
     }
 }
+
+//정점으로부터 모든 경로의 최단 경로 구하기 
