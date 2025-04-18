0d16990429677e8d93c4121e4b11caab0595df4b
Minyoung Kim <mygongjoo@gmail.com>
2025-04-18 14:41:32 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 122.35 ms, Memory: 88.1 MB -BaekjoonHub

--- Java/프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ Java/프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -3,33 +3,43 @@ class Solution
 {
     public int solution(String s){
         
-        char[] str = s.toCharArray();
-        int max = 0;
-        int len = s.length();
-        for(int i=0;i<len;i++){
-            //팰린드롬이 홀수일 경우
-            int left = i-1;
-            int right =i+1;
-            int lenOfOdd = 1;
-            while(left>=0 && right<len&& str[left]==str[right]){
-                lenOfOdd+=2;
-                left--;
-                right++;
-            }
-            //팰린드롬이 짝수일 경우
-            int lenOfEven = 0;
-            if(i+1<len && str[i]==str[i+1]){
-                lenOfEven = 2;
-                int l = i-1;
-                int r = i+2;
-                while(l>=0 && r<len && str[l]==str[r]){
-                    lenOfEven+=2;
-                    l--;
-                    r++;
+        char[] chars = s.toCharArray();
+        
+        // dp[x][y]일 때 팰린드롬인지 아닌지 
+        int[][]dp = new int[s.length()][s.length()];
+        for(int i=0;i<s.length();i++){
+            dp[i][i] = 1;
+        }
+        
+        for(int i=0;i+1<s.length();i++){
+            if(chars[i]==chars[i+1])dp[i][i+1] = 1;
+        }
+        
+        
+        for(int i=2;i<s.length();i++){
+            for(int j=0;j+i<s.length();j++){
+                if(chars[j]==chars[j+i] && dp[j+1][j+i-1]==1){
+                    dp[j][i+j]=1;
                 }
             }
-            max = Math.max(Math.max(lenOfOdd,lenOfEven),max);  
+        }
+        
+        int max = 0;
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<s.length();j++){
+                if(dp[i][j]==1 || dp[j][i]==1){
+                    max = Math.max(max,Math.abs(i-j)+1);
+                } 
+            }
         }
         return max;
+        
+        
     }
+        
 }
+
+//투포인터
+//슬라이딩 윈도우
+
+
