ab4446a6a2a59178f295b9724a3b499495d4ccae
Minyoung Kim <mygongjoo@gmail.com>
2025-05-22 17:34:02 +0900

    [level 3] Title: 단어 변환, Time: 0.99 ms, Memory: 74.1 MB -BaekjoonHub

--- Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ Java/프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,71 +1,46 @@
 import java.util.*;
-import java.util.stream.Collectors;
 class Solution {
-    
-    class Node{
-        int depth;
-        String text;
-        List<String> candidates;
-        
-        Node(int depth,String text,List<String> candidates){
-            this.depth = depth;
-            this.text = text;
-            this.candidates = candidates;
-        }
-    }
-    
     public int solution(String begin, String target, String[] words) {
-        
-        Queue<Node> queue = new LinkedList();
-        
-        List<String> candidates = Arrays
-            .stream(words)
-            .filter(w -> check(begin,w))
-            .collect(Collectors.toList());
-        
-        System.out.println(candidates);
-        
-        for(String candidate : candidates){
-            List<String> newCandidates = new ArrayList<>(Arrays.asList(words));
-            newCandidates.remove(candidate);
-            queue.add(new Node(1,candidate,newCandidates));
+        class Node{
+            int deepth;
+            String value;
+            List<String> candidates;
+            public Node(int deepth,String value, List<String> candidates){
+                this.deepth = deepth;
+                this.value = value;
+                this.candidates = candidates;
+            }
         }
+        Queue<Node> queue = new LinkedList<>();
+    
+        queue.add(new Node(0,begin,Arrays.asList(words)));
         
         while(!queue.isEmpty()){
-            
             Node node = queue.poll();
-            
-            //완성된 경우
-            if(node.text.equals(target)){
-                return node.depth;
+            if(node.value.equals(target)){
+                return node.deepth;
             }
-            //완성되지 않은 경우
-            node.candidates
-                .stream()
-                .filter(c -> check(node.text,c))
-                .forEach(c-> {
-                    List<String> nc3 = new ArrayList<>(node.candidates);
-                    nc3.remove(node.text);
-                    queue.add(new Node(node.depth+1,c,nc3));
-             }) ; 
+            
+            for(String candidate: node.candidates){
+                if(func(node.value,candidate)){
+                    List<String> newCandidates = new ArrayList<>(node.candidates);
+                    newCandidates.remove(candidate);
+                    queue.add(new Node(node.deepth+1,candidate,newCandidates));
+                }
+            }
+            
         }
-                
+        
         int answer = 0;
         return answer;
     }
     
-    public Boolean check(String target, String subject){
+    public Boolean func(String s1, String s2){
+        int dif = 0;
         
-        char[] t = target.toCharArray();
-        char[] s = subject.toCharArray();
-        
-        int num =0;
-        for(int i=0;i<t.length;i++){
-            if(t[i]!=s[i]){num++;}
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i)){dif++;}
         }
-
-        if(num==1){return true;}
-        else{return false;}
-        
+        return dif==1;
     }
 }
