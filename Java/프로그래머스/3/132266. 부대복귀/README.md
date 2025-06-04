732568f1b2562523784c6944d0738346edf8f713
Minyoung Kim <mygongjoo@gmail.com>
2025-06-04 20:03:28 +0900

    [level 3] Title: 부대복귀, Time: 445.07 ms, Memory: 289 MB -BaekjoonHub

--- Java/프로그래머스/3/132266. 부대복귀/README.md
+++ Java/프로그래머스/3/132266. 부대복귀/README.md
@@ -1,10 +1,10 @@
 # [level 3] 부대복귀 - 132266 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266#qna) 
 
 ### 성능 요약
 
-메모리: 234 MB, 시간: 965.08 ms
+메모리: 289 MB, 시간: 445.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 13일 10:10:04
+2025년 06월 04일 20:03:25
 
 ### 문제 설명
 
--- Java/프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ Java/프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,39 +1,58 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int n, int[][] roads, int[] sources, int destination) {   
-        int MAX = 1000000000;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,MAX);
-        dp[destination] = 0;
-        int[] visit = new int[n+1];
-        ArrayList<Integer>[] way = new ArrayList[n+1]; 
-        for(int i=0;i<=n;i++){
-            way[i] = new ArrayList<Integer>();
+    public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        //최대 거리로 기본 세팅
+        int[] distance = new int[n+1];
+        for(int i=1;i<=n;i++){
+            distance[i] = Integer.MAX_VALUE;
         }
+        distance[destination] = 0;
         
+        //양방향 그래프
+        Map<Integer,List<Integer>> map = new HashMap<>();
+        
+        // 인접 노드 세팅
         for(int[] road : roads){
-            way[road[0]].add(road[1]);
-            way[road[1]].add(road[0]);
+            int s1 = road[0];
+            int s2 = road[1];
+            List<Integer> temp1 = map.getOrDefault(s1,new ArrayList<>());
+            temp1.add(s2);
+            map.put(s1,temp1);
+            List<Integer> temp2 = map.getOrDefault(s2,new ArrayList<>());
+            temp2.add(s1);
+            map.put(s2,temp2);
         }
         
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(destination);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+        (a,b) -> distance[a] - distance[b]);
         
-        while(queue.size()!=0){
-            int node = queue.poll();
-            visit[node] = 1;
-            for(Integer adj : way[node]){
-                if(visit[adj]==1){continue;}
-                dp[adj] = Math.min(dp[adj], dp[node]+1);
-                queue.add(adj);
+        pq.addAll(map.get(destination));
+        for(int i : map.get(destination)){
+            distance[i]=1;
+        }
+        
+        int[] visited=new int[n+1];
+        while(!pq.isEmpty()){
+            
+            Integer now = pq.poll();
+            if(visited[now]==1){continue;}
+            visited[now] = 1;
+
+            for(int next : map.get(now)){
+                if(visited[next]==1){continue;}
+                if(distance[next]> distance[now]+1){
+                    distance[next] = distance[now]+1;
+                }
+                pq.add(next);
             }
         }
         
-        int[] answer = new int[sources.length];
+        int[] result = new int[sources.length];
         for(int i=0;i<sources.length;i++){
-            answer[i] = dp[sources[i]]==MAX?-1:dp[sources[i]];
+            int dist = distance[sources[i]];
+            result[i] = dist==Integer.MAX_VALUE?-1:dist;
         }
-
-        return answer;
+    
+        return result;
     }
 }
